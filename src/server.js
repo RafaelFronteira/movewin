218f9c19cd72c51fb434fab7249fde27644e2487
Vitor <hethell7@gmail.com>
2019-05-19 06:21:52 -0300

    port fixed

--- src/server.js
+++ src/server.js
@@ -2,13 +2,12 @@ const express = require('express');
 // const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./api/routes/routes');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV || 'development';
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
-// server.use(express.static(path.join(__dirname, './view')));
 routes.set(server);
 
 // console.log('Uber => ', );
