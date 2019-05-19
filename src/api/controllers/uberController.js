17fc5a5ce26b7098c820e26fcb49439e529c37fc
Vitor <hethell7@gmail.com>
2019-05-19 06:16:01 -0300

    save

--- src/api/controllers/uberController.js
+++ src/api/controllers/uberController.js
@@ -6,7 +6,7 @@ module.exports = {
             client_id: 'WpGOnRIpbOQwc1CG5dtfain9hjXsS8Dp',
             client_secret: '0vQZLvcWYoxd7jgu7DMHTtOwmit8U6dNv9yrAXLU',
             server_token: 'U50cRdRq7eUzaDV1IEvPXk5m8_kOD51RRGgGcT9r',
-            redirect_uri: 'http://localhost:3000/api/auth',
+            redirect_uri: ' https://movewin.herokuapp.com/api/auth',
             name: 'MoveWin',
             language: 'pt_BR',
             sandbox: true
